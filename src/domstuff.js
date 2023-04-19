a4642f36b9b862d86675e04604b733688ae5d8a7
Adi <adithiyans123@gmail.com>
2023-04-19 19:44:35 +0530

    created a default to do project

--- src/domstuff.js
+++ src/domstuff.js
@@ -6,6 +6,32 @@ const putProj = function(proj){
     pdiv.classList.add('projediv');
     pdiv.innerHTML = proj[0].title;
     maindiv.appendChild(pdiv);
+
+
+    const detdiv = document.querySelector('.jobs');
+    let ddiv = document.createElement("div");
+    ddiv.classList.add('job');
+    let ddiv1 = document.createElement("div");
+    let ddiv2= document.createElement("div");
+    let ddiv3 = document.createElement("div");
+    let ddiv4 = document.createElement("div");
+
+    ddiv1.innerHTML = proj[0].title;
+    ddiv2.innerHTML = proj[0].details;
+    ddiv3.innerHTML = proj[0].dati;
+    ddiv4.innerHTML = proj[0].priority;
+
+    ddiv.appendChild(ddiv1);
+    ddiv.appendChild(ddiv2);
+    ddiv.appendChild(ddiv3);
+    ddiv.appendChild(ddiv4);
+
+
+    detdiv.appendChild(ddiv);
+
+
+
+
 }
 
 
