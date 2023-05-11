e9774768a00f0b5ed0cab95548cadbe8bb727322
Adi <adithiyans123@gmail.com>
2023-05-11 17:43:01 +0530

    Made the project name clickable to view the list

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { putProj,putDetails,addlis} from "./domstuff";
 const start = function(){
     let defproj = createProj("main");
     let x = createObj("job","to code",'2023-09-09',"high");
-    addToProj(defproj,x);
+    addToProj(defproj,x,"main");
     let y = createObj("job","to not code",'2023-09-09',"high");
     addToProj(defproj,y,"main");
     putProj(defproj,"main");
